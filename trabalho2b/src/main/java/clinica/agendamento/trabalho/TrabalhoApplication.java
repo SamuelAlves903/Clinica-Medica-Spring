521c1cbee18eadaff4ea824b291bfd4d183c0517
Wellington <wellingtonjoaquim736@gmail.com>
2025-06-01 06:49:33 -0300

    Estilização do Paciente

--- trabalho2b/src/main/java/clinica/agendamento/trabalho/TrabalhoApplication.java
+++ trabalho2b/src/main/java/clinica/agendamento/trabalho/TrabalhoApplication.java
@@ -1,7 +1,14 @@
 package clinica.agendamento.trabalho;
 
+import clinica.agendamento.trabalho.model.Paciente;
+import clinica.agendamento.trabalho.service.PacienteService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TrabalhoApplication {
@@ -9,4 +16,20 @@ public class TrabalhoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrabalhoApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(PacienteService pacienteService) {
+		return args -> {
+			var paciente = new Paciente(
+					null,
+					"Wellington",
+					"10345673456",
+					LocalDate.of(2004, 6, 15),
+					"4498765467"
+			);
+
+			pacienteService.salvar(paciente);
+		};
+	}
+
 }
